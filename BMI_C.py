47a0ef2a67423f0c6985b2a998ea610085eaf152
Mustafa-Karaaslan <karaaslanlar.mustafa@gmail.com>
2024-03-14 11:22:34 +0300

    save 4

--- BMI_C.py
+++ BMI_C.py
@@ -5,8 +5,7 @@ window.title("BMI Calculator")
 window.minsize(width=350,height=300)
 font_1= ("Bold",20,"italic")
 font_2= ("Bold",20)
-font_3= ("bold",25)
-
+font_3= ("bold",20)
 
 height_text = Label(text="Enter your height(cm):", fg="white",font=font_1)
 height_text.config(pady=8)
@@ -22,31 +21,34 @@ weight_text.pack()
 weight_entry = Entry()
 weight_entry.pack()
 
-
 def click_button():
-    Heigh = int(height_entry.get())
-    weight = int(weight_entry.get())
-    BMI = weight / ((Heigh/100)*(Heigh/100))
-    if 0< BMI < 18.5:
-        text= "Underweight" #230
-        He=230
-    elif 18<= BMI <25:
-        text= "Normal"  #169
-        He=169
-    elif 25<= BMI <30:
-        text= "Overweight"#216
-        He=216
-    elif 30<= BMI:
-        text= "Obese" #162
-        He=162
-    elif BMI <= 0:
-        text="entered the wrong number."
-    calculated_BMI_label = Label(text=f"you are {text}",font=font_3)
-    calculated_BMI_label.config(pady=10)
-    calculated_BMI_label.place(x= 175-He/2,y= 190)
-    def how_much_pixel_2():
-        calculated_BMI_label.update()
-        print(calculated_BMI_label.winfo_width())
+    while True:
+        try:
+            Heigh = int(height_entry.get())
+            weight = int(weight_entry.get())
+            BMI = weight / ((Heigh/100)*(Heigh/100))
+            BMI = int(BMI * 100) / 100
+            if 0< BMI < 18.5:
+                text= "Underweight" #230
+                He=230
+            elif 18<= BMI <25:
+                text= "Normal weight"  #169
+            elif 25<= BMI <30:
+                text= "Overweight"#216
+            elif 30<= BMI:
+                text= "Obese" #162
+            elif BMI <= 0:
+                text="entered the wrong number."
+            calculated_BMI_label = Label(text=f"Your BMI is {BMI} you are {text}",font=font_3)
+            calculated_BMI_label.config(pady=10)
+            calculated_BMI_label.pack()
+            break
+        except ValueError:
+            calculated_ValueError = Label(text="please enter a correct value!!..",font=font_3)
+            calculated_ValueError.config(pady=10)
+            calculated_ValueError.pack()
+
+
 calculate_button = Button(text="Calculate",command=click_button)
 calculate_button.place(x= 129,y= 150)
 
